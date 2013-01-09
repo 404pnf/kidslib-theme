fa7f5b0b11c6cf92ef52b1fa5afe2b09c7c2779b
404 <404@example.com>
2013-01-09 17:21:20 +0800

    make the div as little as possible

---
+++ templates/tmp-overwrite/page--user--login.tpl.php
@@ -0,0 +1,8 @@
+
+<div class="wrapper">
+ <?php print render($page['highlighted']); ?>
+ <?php print $messages; ?>
+ <?php print render($page['help']); ?>
+ <?php print render($page['content']); ?>
+
+</div>
