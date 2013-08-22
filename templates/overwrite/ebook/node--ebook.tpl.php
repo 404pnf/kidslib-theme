a9a49f89fa6a798bd6df40ce88fa68ef6ea4e0e5
404 <404@example.com>
2013-08-22 14:45:34 +0800

    让node--ebook.tpl.php看着清楚一点

--- templates/overwrite/ebook/node--ebook.tpl.php
+++ templates/overwrite/ebook/node--ebook.tpl.php
@@ -1,165 +1,102 @@
 <script src="/sites/all/themes/ilearning/booklet/jquery.min.js" type="text/javascript"></script>
 <script src="/sites/all/themes/ilearning/booklet/jquery-ui.min.js" type="text/javascript"></script>
-<script src="/sites/all/libraries/jplayer/jquery.jplayer.min.js" type="text/javascript" ></script> 
+<script src="/sites/all/libraries/jplayer/jquery.jplayer.min.js" type="text/javascript" ></script>
 <script src="/sites/all/themes/ilearning/booklet/jquery-ui-1.8.21.custom.min.js" type="text/javascript"></script>
 <script src="/sites/all/themes/ilearning/booklet/jquery.booklet.1.4.0.js" type="text/javascript"></script>
 <script src="/sites/all/themes/ilearning/booklet/jquery.easing.1.3.js" type="text/javascript"></script>
 <script src="/sites/all/themes/ilearning/booklet/jquery.booklet.1.4.0.min.js" type="text/javascript"></script>
 <link href="/sites/all/themes/ilearning/booklet/jquery.booklet.1.4.0.css" type="text/css" rel="stylesheet" media="screen, projection, tv" />
- 
+
 <link href="/sites/all/themes/ilearning/css/ebook_jplayer.css" rel="stylesheet" type="text/css" />
 <script src="/sites/all/themes/ilearning/js/jplayer_book.js" type="text/javascript"></script>
 <script type="text/javascript">
-$(function() {
-	var arr = $("#imgsize").html();
-	var bookSize = eval(arr);
-	var bookletHeight = 530*bookSize[1]/bookSize[0];
-  //single book
-    $('#ebook-wrapper').booklet({
-	width:  1060,
-	height: bookletHeight,
-	pageNumbers: false,
-	arrows: true,
-	closed: true,
-	autoCenter: true,
-	pagePadding: 0,
-    change: function(event, data) {
-          changeAudioUrl(data.index);
-          } 				
-   });
-	
-
-});
-
-$(document).ready(function(){
-
-	$("#jquery_jplayer_1").jPlayer({
-		swfPath: "/sites/all/libraries/jplayermp3/Jplayer.swf",
-		supplied: "mp3",
-		wmode: "window"
+	$(function() {
+		var arr = $("#imgsize").html();
+		var bookSize = eval(arr);
+		var bookletHeight = 530*bookSize[1]/bookSize[0];
+	  //single book
+		$('#ebook-wrapper').booklet({
+		width:  1060,
+		height: bookletHeight,
+		pageNumbers: false,
+		arrows: true,
+		closed: true,
+		autoCenter: true,
+		pagePadding: 0,
+		change: function(event, data) {
+			  changeAudioUrl(data.index);
+			  }
+	   });
 	});
 
-	
-});
-
+	$(document).ready(function(){
+		$("#jquery_jplayer_1").jPlayer({
+			swfPath: "/sites/all/libraries/jplayermp3/Jplayer.swf",
+			supplied: "mp3",
+			wmode: "window"
+		});
+	});
 </script>
 
-
-
-
-<div id="node-<?php print $node->nid; ?>"
-	class="<?php print $classes; ?> clearfix" <?php print $attributes; ?>>
-
-  <?php print render($title_prefix); ?>
-  <?php if (!$page): ?>
-    <h2 <?php print $title_attributes; ?>>
-		<a href="<?php print $node_url; ?>"><?php print $title; ?></a>
-	</h2>
-  <?php endif; ?>
-  <?php print render($title_suffix); ?>
-
-  <?php if ($display_submitted): ?>
-    <div class="meta submitted">
-      <?php print $user_picture; ?>
-      <?php print $submitted; ?>
-    </div>
-  <?php endif; ?>
-
-
   <div id="ebook-wrapper">
-     
-    <?php
-      // We hide the comments and links now so that we can render them later. 
-      hide($content['comments']);
-      hide($content['links']);
-      //print render($content);
-     
- 
-      $page_counter = count($node->field_page_content['und']);
-      //print $page_counter;
-      $new_page_counter = $page_counter-1;
-      
-    ?>
-    
+    	<?php
+      	$page_counter = count($node->field_page_content['und']);
+     	 //print $page_counter;
+      	$new_page_counter = $page_counter-1;
+    	?>
    <?php
-       
-           
            global $base_url;
-           $arr=array();      
-           $arrimg = array(); 
-           
+           $arr=array();
            //pager_counter为计算的总页数
-           for($item_count=0;$item_count < $page_counter;$item_count++){ 
+           for($item_count=0;$item_count < $page_counter;$item_count++){
+            	foreach($content['field_page_content'][$item_count]['entity']['field_collection_item'] as $field_collection_item)  {
+            	 	$ebook_url = render($field_collection_item['field_page_image']);
+            	 	print "<div class='ebook_content'>
+            	 				<span class= 'ebook_image'><img src=$ebook_url width=530px /></span>";
 
-            foreach($content['field_page_content'][$item_count]['entity']['field_collection_item'] as $field_collection_item)  {
-              print '<div class="ebook_content">';  
-            	 $ebook_image = '<span class= "ebook_image">';
-            	 $ebook_url = render($field_collection_item['field_page_image']);
-            	 $ebook_image .= '<img src='.$ebook_url.' width=530px />';
-            	 $ebook_image .= '</span>';
-            
-            	 print $ebook_image;
-                 $pnumber = render($field_collection_item['field_page_number']);
-                              
-                 
-                 //if (!empty ($field_collection_item['field_page_audio'])){   
-                 $ebook_audio = render($field_collection_item['field_page_audio']);
-                             
-                     //print '<span class="ebook_audio">';                             	      	
-                     //print render($field_collection_item['field_page_audio']);    
-                     //print  '</span>';
-                 $ebooksize =  getimagesize(render($field_collection_item['field_page_image']));
-                 //追进页码和音频到空数组中
-                 array_push($arr,$pnumber,$ebook_audio);
-            }
-         $pnumber = $pnumber-1;  
-         if($pnumber != 0){   
-           if ($pnumber%2==0){
-           	$new_pager = '<div class="right_pager">';
-           	$new_pager .= ''.$pnumber.' / '.$new_page_counter.'';
-           	$new_pager .= '</div>';       
-           }else{
-           	$new_pager = '<div class="left_pager">';
-           	$new_pager .= ''.$pnumber.' / '.$new_page_counter.'';
-           	$new_pager  .= '</div>';
-           }
-         }else{
-         	$new_pager = '';
-         }  
-           print $new_pager;  
-        
-        print '</div>';
+                 	$pnumber = render($field_collection_item['field_page_number']);
+                 	$ebook_audio = render($field_collection_item['field_page_audio']);
+                	$ebooksize =  getimagesize(render($field_collection_item['field_page_image']));
+                 	//追进页码和音频到空数组中
+                 	array_push($arr,$pnumber,$ebook_audio);
+            		}
+        		   $pnumber = $pnumber-1;
+       				  if($pnumber != 0){
+          	 				if ($pnumber%2==0){
+          	 					print "<div class='right_pager'> $pnumber / $new_page_counter</div>";
+          						 }
+          	 				else{
+          	 					print "<div class='left_pager'> $pnumber / $new_page_counter</div>";
+          						 }
+        			 		}
+         			else{
+         					$new_pager = '';
+         			}
+        		print $new_pager;
+        		print '</div>'; /* end ebook_content ? */
         }
            //每张图片的具体长度和宽度
-    ?>
-  
-  </div>
- 
-    <?php print $links; ?>
+		?>
+</div>
 
-    <div class="link-wrapper">
+<div class="mp3-player">
       <?php
+          $arrimg = array();
            array_push($arrimg,$ebooksize[0],$ebooksize[1]);
-           $outputimg = '<div id="imgsize"style="display:none">';
-           $outputimg .= json_encode($arrimg);
-           $outputimg .='</div>';
-           echo $outputimg;
-      
-           $outputjs = '<div id="audio_data" style="display:none">';
-           $outputjs .= '<div id="audio_path">'; 
-           //$outputjs .= 'http://ilearning.cn/sites/default/files/ebook/pageaudio/';
-           $outputjs .= $base_url.'/sites/default/files/ebook/pageaudio/';          
-           $outputjs .= '</div>';
-           $outputjs .= '<div id="audio_name">';
-           //$outputjs .=json_encode(str_replace('http://ilearning.cn/sites/default/files/ebook/pageaudio/','',$arr));           
-           $outputjs .=json_encode(str_replace($base_url.'/sites/default/files/ebook/pageaudio/','',$arr));
-           $outputjs .= '</div>';
-           $outputjs .='</div>';
-           echo  $outputjs;
-       ?>
-      <div id="jquery_jplayer_1" class="jp-jplayer"></div>
+           $img_url = json_encode($arrimg);
+           print "<div id='imgsize'style='display:none'>$img_url</div>";
 
-		<div id="jp_container_1" class="jp-audio">
+           $audio_path = $base_url.'/sites/default/files/ebook/pageaudio/';
+           $audio_name = json_encode(str_replace($base_url.'/sites/default/files/ebook/pageaudio/','',$arr));
+           print "<div id='audio_data' style='display:none'>
+           				<div id='audio_path'>$audio_path</div>
+           				<div id='audio_name'>$audio_name</div>
+           			</div>";
+       ?>
+
+	  <?php // mp3播放器及其样式，不要删除 ?>
+      <div id="jquery_jplayer_1" class="jp-jplayer"></div>
+	  <div id="jp_container_1" class="jp-audio">
 			<div class="jp-type-single">
 				<div class="jp-gui jp-interface">
 					<ul class="jp-controls">
@@ -174,20 +111,19 @@ $(document).ready(function(){
 							<div class="jp-play-bar"></div>
 						</div>
 					</div>
-					
+
 					<div class="jp-volume-bar">
 						<div class="jp-volume-bar-value"></div>
 					</div>
-					
+
 					<div class="jp-time-holder">
 						<div class="jp-current-time"></div>
 						<div class="jp-duration"></div>
 					</div>
-				</div>
-			</div>
-		</div>
-      
-    </div>
-  <?php print render($content['comments']); ?>
-  
-</div>
+
+				</div> <!-- end jp-gui  -->
+			</div> <!-- ends jp-type-single -->
+		</div> <!-- ends jp_container_1 -->
+
+</div><!-- ends mp3-player-->
+
