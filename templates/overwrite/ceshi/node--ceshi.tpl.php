aeaaed840be54a0ed509d049d207dbcdb005d329
404 <404@example.com>
2013-08-23 09:55:04 +0800

    让代码更规范些

--- templates/overwrite/ceshi/node--ceshi.tpl.php
+++ templates/overwrite/ceshi/node--ceshi.tpl.php
@@ -7,7 +7,7 @@
 <script type="text/javascript" src="/sites/all/themes/ilearning/js/hiddenShow.js"></script>
 <script type="text/javascript">
 	function onclick_right(a){
-		for(var i=1;i<=4;i++){
+		for(var i = 1; i <= 4; i++){
 			document.getElementById("ceshi_an_word_"+i).className="u_bg"
 			}
 		var an = document.getElementById("answer");
@@ -18,7 +18,7 @@
 		an_word.className="answer_word_r answer_word fleft";
 	}
 	function onclick_wrong(b){
-		for(var i=1;i<=4;i++){
+		for(var i = 1; i <= 4; i++){
 			document.getElementById("ceshi_an_word_"+i).className="u_bg"
 			}
 		var an = document.getElementById("answer");
@@ -55,7 +55,7 @@
 	<div class="title">
         <a class="font_DFHaibaoW12 title_left">测一测</a>
 		<a class="title_name font_DFHaibaoW12">
-		    <?php
+		    <?php //显示测试节点的题目
 		         $unit =  views_embed_view("xinimage","unit",$node->nid);
 		         print $unit;
 		     ?>
@@ -71,7 +71,7 @@
     <div class="fleft">
     	<div class="word_box">
 			<ul id="word_list_a">
-				<?php
+				<?php // 选项区，有4个选项
 					for ($i = 0; $i <= 3; $i++) {
 						$word = $node->field_xuanxiang['und'][$i]['value'];
 						$len = strlen($word);
@@ -85,49 +85,7 @@
 				?>
        		 </ul>
 		</div>
-
-        <?php /* 这个代码太不DRY了！！！上面的替代它了
-           //需要判断选项的长短,最长的单词为10个字母,最长的短语加上空格为14个字母
-           $an0=$node->field_xuanxiang['und'][0]['value'];
-
-           if(strlen($an0)>=10&&strlen($an0)<=16){
-               $answer0 ='<li class="toolong"><a id="ceshi_an_word_1" onclick="onclick_right(this);" href="#">'.$an0.'</a></li>';
-             }else{
-               $answer0 = '<li><a  id="ceshi_an_word_1" onclick="onclick_right(this);" href="#">'.$an0.'</a></li>';
-             }
-
-           $an1=$node->field_xuanxiang['und'][1]['value'];
-           if(strlen($an1)>=10&&strlen($an1)<=16){
-               $answer1 ='<li class="toolong"><a  id="ceshi_an_word_2" onclick="onclick_wrong(this);" href="#">'.$an1.'</a></li>';
-            }else{
-           	   $answer1 ='<li><a  id="ceshi_an_word_2" onclick="onclick_wrong(this);" href="#">'.$an1.'</a></li>';
-            }
-
-           $an2=$node->field_xuanxiang['und'][2]['value'];
-           if(strlen($an2)>=10&&strlen($an2)<=16){
-           	$answer2 ='<li class="toolong"><a  id="ceshi_an_word_3" onclick="onclick_wrong(this);" href="#">'.$an2.'</a></li>';
-           }else{
-           	$answer2 ='<li><a  id="ceshi_an_word_3" onclick="onclick_wrong(this);" href="#">'.$an2.'</a></li>';
-           }
-
-           $an3=$node->field_xuanxiang['und'][3]['value'];
-           if(strlen($an3)>=10&&strlen($an3)<=16){
-           		$answer3 ='<li class="toolong"><a  id="ceshi_an_word_4" onclick="onclick_wrong(this);" href="#">'.$an3.'</a></li>';
-           		}
-           else {
-           		$answer3 ='<li><a  id="ceshi_an_word_4" onclick="onclick_wrong(this);" href="#">'.$an3.'</a></li>';
-           }
-
-           $arr=array($answer0,$answer1,$answer2,$answer3);
-           shuffle($arr); // php中直接就改变了$arr了！
-           //print_r ($arr);
-           foreach ($arr as $output) {
-           		echo $output; }
-        */
-         ?>
-
-     <!---选项区结束---->
-     <!---播放器区---->
+		<! -- 播放器区 -->
 		<div class="op_box">
 			<div class="oh">
 				<div id="jquery_jplayer_1" class="jp-jplayer"></div>
@@ -158,36 +116,25 @@
            <?php
 			    $audio_url = file_create_url ( $node->field_ceshi_audio ['und'] [0] ['uri'] );
                 $audio_name = $node->field_ceshi_audio['und'][0]['filename'];
-                $audio_mp3 .="<a id='audio_player' href="."$audio_url"."></a>";
-                print  $audio_mp3;
+                print "<a id='audio_player' href='$audio_url'></a>"
             ?>
-
        </div>
 			<div class="fclear oh">
+				<?php
+							 //$targetid=$node->field_ref_to_nse['und']['0']['target_id'];
+							 //print $target_id;
+							 $newer = node_sibling_nse($node,'next',NULL,NULL,NULL);
+							 $older = node_sibling_nse($node,'prev',NULL,NULL,NULL);
 
-
-        <?php
-                     //$targetid=$node->field_ref_to_nse['und']['0']['target_id'];
-                     //print $target_id;
-                     $newer = node_sibling_nse($node,'next',NULL,NULL,NULL);
-                     $older = node_sibling_nse($node,'prev',NULL,NULL,NULL);
-
-                     if (!empty ($older)){
-                        $output_older ="<a class='pre font_mrosoftYHB fleft' href="."$older".">".上一题."</a>";
-
-                     }
-                     print $output_older;
-                     if (!empty($newer)){
-                        $output_newer ="<a class='next font_mrosoftYHB fleft' href="."$newer".">".下一题."</a>";
-
-                     }
-                     print $output_newer;
-
-         ?>
-        </div>
-
-		</div>
-
+							 if (!empty ($older)){
+								print "<a class='pre font_mrosoftYHB fleft' href='$older'>上一题</a>";
+								}
+							 if (!empty($newer)){
+								print "<a class='pre font_mrosoftYHB fleft' href='$newer'>下一题</a>";
+							 }
+				 ?>
+        	</div>
+	 </div>
     </div>
 	<div class="question fleft">
     <?php  print render($content['field_answer_image']); ?>
