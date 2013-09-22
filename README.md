d1a41208573069b7ee9528e1afad6c8237275a76
404 <404@example.com>
2013-09-22 15:01:15 +0800

    改写分类页面video类型链接地址

---
+++ README.md
@@ -0,0 +1,2 @@
+修改分类页面的图片连接到哪里的views在
+templates/overwrite/ebook/ebook_list_category/views-view-fields--ebook-list-category--default.tpl.php
--- templates/overwrite/ebook/ebook_list_category/views-view-fields--ebook-list-category--default.tpl.php
+++ templates/overwrite/ebook/ebook_list_category/views-view-fields--ebook-list-category--default.tpl.php
@@ -1,39 +1,9 @@
-<?php
-
-/**
- * @file
- * Default simple view template to all the fields as a row.
- *
- * - $view: The view in use.
- * - $fields: an array of $field objects. Each one contains:
- *   - $field->content: The output of the field.
- *   - $field->raw: The raw data for the field, if it exists. This is NOT output safe.
- *   - $field->class: The safe class id to use.
- *   - $field->handler: The Views field handler object controlling this field. Do not use
- *     var_export to dump this object, as it can't handle the recursion.
- *   - $field->inline: Whether or not the field should be inline.
- *   - $field->inline_html: either div or span based on the above flag.
- *   - $field->wrapper_prefix: A complete wrapper containing the inline_html to use.
- *   - $field->wrapper_suffix: The closing tag for the wrapper.
- *   - $field->separator: an optional separator that may appear before a field.
- *   - $field->label: The wrap label text to use.
- *   - $field->label_html: The full HTML of the label to use including
- *     configured element type.
- * - $row: The raw result object from the query, with all data it fetched.
- *<img src="<?php print base_path().path_to_theme(); ?>/images/tv_0<?php print $fields['counter']->content; ?>.png" /><?php print $fields['title']->content;?>
- * @ingroup views_templates
- */
-?>
 <li class="<?php print $fields['type']->content; ?>">
-<?php
-
- ?>
   <div class="big_image"><?php print $fields['title']->content; ?></div>
 <?php 
-
-    $bg_arr = ilearning_get_shuffle_array();
-    $type = $fields['type']->content;
-    $counter =  $fields['counter']->content;
+  $bg_arr = ilearning_get_shuffle_array();
+  $type = $fields['type']->content;
+  $counter =  $fields['counter']->content;
 	$rows = 15;
 	$page = isset($_REQUEST['page']) ? $_REQUEST['page'] : 0;
 	$counter = $counter - $rows * $page;
@@ -41,18 +11,22 @@
     $base_path =  base_path();
     $theme_path = path_to_theme();
     if ($type == "video"){
+	    $result = views_embed_view('get_video_vid', 'default', $fields['nid']->content);
+      $video_id = trim(strip_tags($result));
+		  $name = trim(strip_tags($name));
+		  $name = '<span>' .l($name, 'video/' . $video_id . '.html') .'</span>';
      	$output .= '<img src="';	
- 		$output .= $base_path.$theme_path.'/images/tv_'.$bg_arr[$counter-1].'.png"/>'.$name;
+ 		  $output .= $base_path.$theme_path.'/images/tv_'.$bg_arr[$counter-1].'.png"/>'.$name;
     }
 	elseif ($type == "book"){
 	    $result = views_embed_view('get_book_id', 'default', $fields['nid']->content);
-        $book_id = trim(strip_tags($result));
-       //print $fields['nid']->content;
-        //print $book_id;
-		$name = trim(strip_tags($name));
-		$name = '<span>' .l($name, 'sites/default/files/bilingual/' . $book_id . '.html') .'</span>';
+      $book_id = trim(strip_tags($result));
+      //print $fields['nid']->content;
+      //print $book_id;
+		  $name = trim(strip_tags($name));
+		  $name = '<span>' .l($name, 'bilingual/' . $book_id . '.html') .'</span>';
      	$output .= '<img src="';	
- 		$output .= $base_path.$theme_path.'/images/book_'.$bg_arr[$counter-1].'.png"/>'.$name;
+ 		  $output .= $base_path.$theme_path.'/images/book_'.$bg_arr[$counter-1].'.png"/>'.$name;
     }
 	else{  
     	$output .= '<img src="';
@@ -60,8 +34,4 @@
     }
    print $output;  
 ?>
-
-
 </li>
-
-
