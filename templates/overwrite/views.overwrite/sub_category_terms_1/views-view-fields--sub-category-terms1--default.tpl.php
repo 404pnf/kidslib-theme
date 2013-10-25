cbaa947d6b3215b1b44e8f68a63f8674d2d1fb9d
404 <404@example.com>
2013-10-25 17:46:42 +0800

    搞定了当$age不是出现在url最后的时候不应该带.html后缀的问题

--- templates/overwrite/views.overwrite/sub_category_terms_1/views-view-fields--sub-category-terms1--default.tpl.php
+++ templates/overwrite/views.overwrite/sub_category_terms_1/views-view-fields--sub-category-terms1--default.tpl.php
@@ -1,14 +1,15 @@
 <li>
 <?php 
       $category = arg(2);
-      $age = arg(3);
+      // 因为arg(3)已经被改写有.html后缀了。这里$age出现在第3位且会有第4位因此去掉.html
+      $age = str_replace(".html", "", arg(3));
       $tid = $row->tid;
       $name = $fields['name']->content;
       if (!empty($age)){
-          print l($name,'ebooklist/category/'.$category.'/'.$age.'/'.$tid . '.html');
+          print l($name, 'ebooklist/category/'.$category.'/'.$age.'/'.$tid . '.html');
       }else{
           //print l($name,'ebooklist/category/'.$category.'/1/'.$tid );
-          print l($name,'ebooklist/category/'.$category.'/1/'.$tid );
+          //print l($name,'ebooklist/category/'.$category.'/1/'.$tid );
       } 
       print $output;  
 ?>
