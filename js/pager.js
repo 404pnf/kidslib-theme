66bd8a1fd8751a17bc5f78dcffba9e34adc9ae3d
404 <404@example.com>
2013-10-29 14:48:10 +0800

    add pager.js; add pager div in layouts/classify/panels- ..

---
+++ js/pager.js
@@ -0,0 +1,91 @@
+var record = 18;//每页显示多少条记录
+var count = 1;//记录总数
+var pageTotal = 1//总页数
+var pagenum = 1;//将要显示的页码
+
+//重新整理当前页码，如果页面小于1，则赋值为1，如果大于总页数，则等于总页数
+coordinatePagenum(pagenum);
+
+//根据当前要显示的页码取得当前面里第一条记录的号码
+var pageBegin = (record*(pagenum-1)+1)|0;
+
+//根据当前要显示的页码取得当前面里最后一条记录的号码
+var pageEnd = record*pagenum;
+
+function initdata(pagenum)
+{
+    var list_obj = document.getElementById("list").getElementsByTagName("li");
+    count=list_obj.length;
+    pageTotal = ((count+record-1)/record)|0;//总页数
+    showhiddenRecord(pagenum)
+}
+function showhiddenRecord(pagenum){
+    //document.getElementById('number').innerHTML=pagenum;
+    if(pagenum<=1){
+       document.getElementById('theFirstPage').innerHTML="<span class=\"pager-none\">第一页</span>";
+        document.getElementById('thePrePage').innerHTML="<span  class=\"pager-none\">上一页</span";
+    }else{
+        document.getElementById('theFirstPage').innerHTML="<a href=\"javascript:firstPage()\" class=\"pager-first\">第一页</a>";
+        document.getElementById('thePrePage').innerHTML="<a href=\"javascript:prePage()\"  class=\"pager-previous\">上一页</a>";
+    }
+    if(pagenum>=pageTotal){
+        document.getElementById('theNextPage').innerHTML="<span  class=\"pager-none\">下一页</span>";
+        document.getElementById('theLastPage').innerHTML="<span  class=\"pager-none\">最后一页</span";
+    }else{
+        document.getElementById('theNextPage').innerHTML="<a href=\"javascript:nextPage()\"  class=\"pager-next\">下一页</a>";
+        document.getElementById('theLastPage').innerHTML="<a href=\"javascript:lastPage()\"  class=\"pager-last\">最后一页</a>";
+    }
+    //document.getElementById('mySelect').value=pagenum;
+    //根据当前要显示的页码取得当前面里第一条记录的号码
+    pageBegin = (record*(pagenum-1)+1)|0;
+    
+    //根据当前要显示的页码取得当前面里最后一条记录的号码
+    pageEnd = record*pagenum;
+    list_obj = document.getElementById("list").getElementsByTagName("li");
+    for(var i=0;i<list_obj.length;i++){
+        if(i>=pageBegin-1 && i<=pageEnd-1){
+           list_obj[i].style.display="";
+        }else{
+           list_obj[i].style.display="none";
+        }
+    }
+    
+}
+
+function firstPage(){
+    pagenum=1;
+    showhiddenRecord(pagenum);
+}
+
+function lastPage(){
+    pagenum=pageTotal
+    showhiddenRecord(pageTotal);
+}
+
+//重新整理当前页码，如果页面小于1，则赋值为1，如果大于总页数，则等于总页数
+function coordinatePagenum(num){
+    if(num<1){
+        num="1";
+    }else if(num>pageTotal){
+        num=pageTotal;
+    }
+}
+
+function prePage(){
+    pagenum--;
+    coordinatePagenum(pagenum);
+    showhiddenRecord(pagenum);
+}
+
+function nextPage(){
+    pagenum++;
+    coordinatePagenum(pagenum);
+    showhiddenRecord(pagenum);
+}
+
+function gotoPage(num){
+    pagenum=num
+    coordinatePagenum(pagenum);
+    showhiddenRecord(num);
+}
+
--- layouts/classify/panels-classify.tpl.php
+++ layouts/classify/panels-classify.tpl.php
@@ -1,26 +1,10 @@
-<?php
-/**
- * @file
- * Template for a 2 column panel layout.
- *
- * This template provides a two column panel display layout, with
- * each column roughly equal in width.
- *
- * Variables:
- * - $id: An optional CSS id to use for the layout.
- * - $content: An array of content, each item in the array is keyed to one
- *   panel of the layout. This layout supports the following sections:
- *   - $content['left']: Content in the left column.
- *   - $content['right']: Content in the right column.
- */
-?>
 <div class="book_classify">
 <!-- 	顶部开始	 -->
 <div class="top pngFix">
 	<div class="top pngFix">
 		<div class="top_login">
-          <?php print $content['top_login']; ?>     
-         </div>
+         <?php print $content['top_login']; ?>     
+    </div>
 		<div class="title"><?php print $content['title']; ?>  </div>
 	</div>
 	<div class="title">
@@ -44,9 +28,16 @@
     </div>
 	<!--top list end-->
 	<!--book list-->
-	<ul class="book_list">
+	<ul id="list" class="book_list">
 <?php print $content['book_list']; ?>
     </ul>
+	  <!-- 翻页区块 -->
+    <div class="page fclear"> 
+    <span id="theFirstPage" ><a href="javascript:firstPage()">第一页</a></span>
+    <span id="thePrePage"><a href="javascript:prePage()">上一页</a></span>
+    <span id="theNextPage"><a href="javascript:nextPage()">下一页</a></span>
+    <span id="theLastPage"><a href="javascript:lastPage()">最后一页</a></span>
+    </div>
 	<!--book list end-->
 </div>
 
