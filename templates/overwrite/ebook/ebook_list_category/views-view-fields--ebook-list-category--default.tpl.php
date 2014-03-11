d6fd2d465b1f21ca07308cb793d25aa318dac5ea
Your Name <you@example.com>
2014-03-11 14:24:12 +0800

    move assets folder to /asset

--- templates/overwrite/ebook/ebook_list_category/views-view-fields--ebook-list-category--default.tpl.php
+++ templates/overwrite/ebook/ebook_list_category/views-view-fields--ebook-list-category--default.tpl.php
@@ -4,21 +4,17 @@
   $bg_arr = ilearning_get_shuffle_array();
   $type = $fields['type']->content;
 	$rows = 15;
-  //$counter =  $fields['counter']->content % $rows;
   $counter =  ($fields['counter']->content % $rows ) + 1;
-  //print $counter;
 	$page = isset($_REQUEST['page']) ? $_REQUEST['page'] : 0;
 	$counter = $counter - $rows * $page;
     $name = $fields['title']->content;
-    //$base_path =  base_path();
     $base_path =  base_path();
     $theme_path = path_to_theme();
     if ($type == "video"){
 	    $result = views_embed_view('get_video_vid', 'default', $fields['nid']->content);
       $video_id = trim(strip_tags($result));
 		  $name = trim(strip_tags($name));
-		  //$name = '<span>' .l($name, 'video/html/' . $video_id . '.html') .'</span>';
-		  $name = '<span>' .l($name, 'video/html/' . $video_id . '.html',  array('attributes' => array('target' => '_blank'))) .'</span>';
+		  $name = '<span>' .l($name, 'asset/video/html/' . $video_id . '.html',  array('attributes' => array('target' => '_blank'))) .'</span>';
      	$output .= '<img src="';	
  		  $output .= $base_path.$theme_path.'/images/tv_'.$bg_arr[$counter-1].'.png"/>'.$name;
     }
@@ -28,7 +24,7 @@
       //print $fields['nid']->content;
       //print $book_id;
 		  $name = trim(strip_tags($name));
-		  $name = '<span>' .l($name, 'bilingual/' . $book_id . '.html',  array('attributes' => array('target' => '_blank'))) .'</span>';
+		  $name = '<span>' .l($name, 'asset/bilingual/' . $book_id . '.html',  array('attributes' => array('target' => '_blank'))) .'</span>';
      	$output .= '<img src="';	
  		  $output .= $base_path.$theme_path.'/images/book_'.$bg_arr[$counter-1].'.png"/>'.$name;
     }
@@ -36,7 +32,7 @@
 	    $result = views_embed_view('get_ebook_id', 'default', $fields['nid']->content);
       $ebook_id = trim(strip_tags($result));
 		  $name = trim(strip_tags($name));
-		  $name = '<span>' .l($name, 'ebook/html/' . $ebook_id . '.html', array('attributes' => array('target' => '_blank'))) .'</span>';
+		  $name = '<span>' .l($name, 'asset/ebook/html/' . $ebook_id . '.html', array('attributes' => array('target' => '_blank'))) .'</span>';
       //print $ebook_id;
      	$output .= '<img src="';	
  		  $output .= $base_path.$theme_path.'/images/book_'.$bg_arr[$counter-1].'.png"/>'.$name;
@@ -45,7 +41,7 @@
 	    $result = views_embed_view('get_xbz_id', 'default', $fields['nid']->content);
       $xbz_id = trim(strip_tags($result));
 		  $name = trim(strip_tags($name));
-		  $name = '<span>' .l($name, 'xbz/html/' . $xbz_id . '.html', array('attributes' => array('target' => '_blank'))) .'</span>';
+		  $name = '<span>' .l($name, 'asset/xbz/html/' . $xbz_id . '.html', array('attributes' => array('target' => '_blank'))) .'</span>';
       //print $xbz_id;
      	$output .= '<img src="';	
  		  $output .= $base_path.$theme_path.'/images/book_'.$bg_arr[$counter-1].'.png"/>'.$name;
